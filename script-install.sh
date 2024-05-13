366c8ad8454d6e958e8e63f299e6bf3262623efc
MiguelPort80 <163421687+MiguelPort80@users.noreply.github.com>
2024-05-13 20:38:28 -0300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -5,7 +5,7 @@ echo "Instalador dos meus apps"
 sudo apt update
 sudo apt upgrade
 
-sudo apt install rbenv
+
 sudo apt install nano
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
