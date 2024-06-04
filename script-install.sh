1b99f5f4cadf7df1bbec8e4776aada6657f886b0
MiguelPort80 <163421687+MiguelPort80@users.noreply.github.com>
2024-06-04 10:05:42 -0300

    adicionado vim e vundle script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -6,6 +6,7 @@ sudo apt update
 sudo apt upgrade
 
 
-sudo apt install nano
+sudo apt install vim
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
