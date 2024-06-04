2fd1d52be7a16c5197b4000ae62d73aa950d2e5c
MiguelPort80 <163421687+MiguelPort80@users.noreply.github.com>
2024-06-04 10:06:45 -0300

    Versão do script para Solus os

--- script-install.sh
+++ script-install.sh
@@ -2,11 +2,11 @@
 
 echo "Instalador dos meus apps"
 
-sudo apt update
-sudo apt upgrade
+sudo eopkg list-update
+sudo eopkg upgrade
 
 
-sudo apt install vim
-sudo apt install flatpak
+sudo eopkg install vim
+sudo eopkg install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
